e27eb78d2f6b2e1cd5b8f6b667380b81d1e9bcd4
Nonganba <nanganba.th1109@gmail.com>
2022-12-07 16:22:25 +0530

    add SearchExercises and BodyPart

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,30 @@
-import React from 'react'
-import { useState } from 'react'
-import { Box } from '@mui/material'
+import React from "react";
+import { useState } from "react";
+import { Box } from "@mui/material";
 
-import Exercises from '../components/Exercises'
-import SearchExercises from '../components/SearchExercises'
-import HeroBanner from '../components/HeroBanner'
+import Exercises from "../components/Exercises";
+import SearchExercises from "../components/SearchExercises";
+import HeroBanner from "../components/HeroBanner";
 
 const Home = () => {
+  const [exercises, setExercises] = useState([]);
+  const [bodyPart, setBodyPart] = useState("all");
+
   return (
     <Box>
       <HeroBanner />
-      <SearchExercises />
-      <Exercises />
+      <SearchExercises
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+      />
+      <Exercises
+        setExercises={setExercises}
+        bodyPart={bodyPart}
+        setBodyPart={setBodyPart}
+      />
     </Box>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
